9e3c2e4d7ad0374cd4af91028ff2fc91cefe15f7
David Padilla <davidpadilla@Davids-MBP.attlocal.net>
2021-02-22 02:30:37 -0500

    Product Service/Controller Added

---
+++ SolarCoffee.Web/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SolarCoffee.Services.Product;
+
+namespace SolarCoffee.Web.Controllers
+{
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+        private readonly IProductService _productService;
+
+        public ProductController(ILogger<ProductController> logger, IProductService productService)
+        {
+            _logger = logger;
+            _productService = productService;
+        }
+
+        [HttpGet("/api/product")]
+        public ActionResult GetResult()
+        {
+            _logger.LogInformation("Getting All Products...");
+            _productService.GetAllProducts();
+            return Ok("");
+        }
+    }
+}
